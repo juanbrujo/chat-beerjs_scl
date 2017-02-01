3527f184440ed7399f1f78cdbc52783d6b0a17ce
Jaime Guaman <jaime@guaman.cl>
2017-02-01 18:31:18 -0300

    fix(firebase): fucking timestamp

--- ng1_jaimeguaman/src/app/firebase/service.js
+++ ng1_jaimeguaman/src/app/firebase/service.js
@@ -57,9 +57,22 @@
       this.onMessage = function(room, callback){
         firebase.database().ref(getRoomPath(room)).on('child_added', function(message){
           var _message = message.val();
+          _message.timestamp = decode(message.key);
+
           if(_message.timestamp != NaN && _message.timestamp != undefined){
             _message.timestamp = parseFloat(_message.timestamp);
             callback(_message);
+          }
+
+          function decode(id) {
+            var PUSH_CHARS = "-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz";
+            id = id.substring(0,8);
+            var timestamp = 0;
+            for (var i=0; i < id.length; i++) {
+              var c = id.charAt(i);
+              timestamp = timestamp * 64 + PUSH_CHARS.indexOf(c);
+            }
+            return timestamp;
           }
         });
       }
@@ -67,8 +80,7 @@
       this.sendMessage = function(room, message){
         var newMessage = {
           author: _user,
-          text: message,
-          timestamp: new Date().getTime()
+          text: message
         }
 
         firebase.database().ref(getRoomPath(room)).push(newMessage);
